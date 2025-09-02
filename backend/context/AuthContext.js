4759a8ee081254f3af53327d070f088b283018d6
Tarak Khurana <tarakkhurana29@gmail.com>
2025-09-02 18:12:08 +0530

    Updated backend package.json to add build script

---
+++ backend/context/AuthContext.js
@@ -0,0 +1,10 @@
+import { useState,createContext,useContext } from "react";
+
+export default function Hello(){
+    return(
+        <div>
+            <h1>hekaksdjdhakj</h1>
+        </div>
+    )
+}
+
