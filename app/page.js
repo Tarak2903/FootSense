d47e7cbc983c18eddd8e7da215ffab2f346fa1b3
Tarak Khurana <tarakkhurana29@gmail.com>
2025-09-04 17:15:21 +0530

    final

--- app/page.js
+++ app/page.js
@@ -14,7 +14,7 @@ export default function Home() {
         <div className="flex flex-col gap-y-4 border p-10 rounded-2xl border-slate-500">
        <h1 className=" text-3xl">Welcome to FootSense</h1>
         <h2 className="text-2xl flex justify-center">Login as</h2>
-        <div className="flex justify-center gap-x-8">
+        <div className="flex justify-center gap-x-8 scheme-dark">
           
       <button type="button" onClick={()=>hello.push('/Admin')} className="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800 cursor-pointer">Admin</button>
       <button type="button" onClick={()=>hello.push('/Staff')} className="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800 cursor-pointer">Staff</button>
